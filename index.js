9dc69f1d98fe1cd5d14024b06762d7813930f00e
James Ivings <james@squarecat.io>
2023-03-14 09:34:26 +0200

    switch tags for tag

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const core = require('@actions/core');
-const digitalocean = require('digitalocean');
+const core = require("@actions/core");
+const digitalocean = require("digitalocean");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const apiKey = core.getInput('digital-ocean-key');
-  const tags = core.getInput('tags');
+  const apiKey = core.getInput("digital-ocean-key");
+  const tag = core.getInput("tag");
   const client = digitalocean.client(apiKey);
 
-  console.log(`Fetching droplets for tags ${JSON.stringify(tags)}`);
+  console.log(`Fetching droplets for tag ${JSON.stringify(tag)}`);
 
   client.droplets
     .list()
     .then((drops) => {
-      return drops.filter((d) => d.tags.some((dtag) => tags.includes(dtag)));
+      return drops.filter((d) => d.tags.includes(tag));
     })
     .then((drops) => {
       console.log(`Found ${drops.length} matching droplets`);
       return drops.map((d) => d[0].networks.v4[0].ip_address);
     })
     .then((ips) => {
-      core.setOutput('server_ips', ips || []);
+      core.setOutput("server_ips", ips || []);
     })
     .catch((err) => {
       core.setFailed(err.message);
